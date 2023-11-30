341096e8ba3904ae9995e1820ddced6a86e07847
tugceagar <tugceagar@icloud.com>
2023-11-30 15:02:13 +0300

    Account entity was added

---
+++ account/src/main/java/com/folksdev/account/model/Account.java
@@ -0,0 +1,30 @@
+package com.folksdev.account.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    private LocalDateTime creationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
+    private Set<Transaction> transaction;
+
+
+}
