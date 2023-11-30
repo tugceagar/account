fb357546c738edcf50c93f0645f2f000f21485d1
tugceagar <tugceagar@icloud.com>
2023-11-30 15:50:47 +0300

    Create account repository

---
+++ account/src/main/java/com/folksdev/account/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.folksdev.account.repository;
+
+import com.folksdev.account.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,String> {
+
+
+}
