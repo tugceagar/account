d01a3f5a6905fffba201c7b06f1cc8eea5c3c73c
tugceagar <tugceagar@icloud.com>
2024-01-16 16:33:11 +0300

    Add annotations

--- account/src/main/java/com/folksdev/account/dto/CustomerAccountDto.java
+++ account/src/main/java/com/folksdev/account/dto/CustomerAccountDto.java
@@ -1,13 +1,22 @@
 package com.folksdev.account.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class CustomerAccountDto {
     private String id;
     private BigDecimal balance = BigDecimal.ZERO;
     private Set<TransactionDto> transactions;
     private LocalDateTime creationDate;
+
 
 }
--- account/src/main/java/com/folksdev/account/dto/CustomerDto.java
+++ account/src/main/java/com/folksdev/account/dto/CustomerDto.java
@@ -1,11 +1,20 @@
 package com.folksdev.account.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.Set;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDto {
     private String id;
     private String name;
     private String surname;
     private Set<CustomerAccountDto> accounts;
+
+
 }
