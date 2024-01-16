aebe4d193e2276ba62fa027020e49b3d0d225157
tugceagar <tugceagar@icloud.com>
2024-01-16 16:33:59 +0300

    Add convertToCustomerDto method

--- account/src/main/java/com/folksdev/account/dto/CustomerDtoConverter.java
+++ account/src/main/java/com/folksdev/account/dto/CustomerDtoConverter.java
@@ -3,8 +3,15 @@ package com.folksdev.account.dto;
 import com.folksdev.account.model.Customer;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
 public class CustomerDtoConverter {
+    private final CustomerAccountDtoConverter converter;
+
+    public CustomerDtoConverter(CustomerAccountDtoConverter converter) {
+        this.converter = converter;
+    }
 
     public AccountCustomerDto convertToAccountCustomer(Customer from) {
         if (from == null) {
@@ -13,5 +20,13 @@ public class CustomerDtoConverter {
         return new AccountCustomerDto(from.getId(),
                 from.getName(),
                 from.getSurname());
+    }
+
+    public CustomerDto convertToCustomerDto(Customer from) {
+       return new CustomerDto(
+               from.getId(),
+               from.getName(),
+               from.getSurname(),
+               from.getAccounts().stream().map(converter::convert).collect(Collectors.toSet()));
     }
 }
