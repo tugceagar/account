a83797e73d442d8ece6a150bccd5b0eea4414161
tugceagar <tugceagar@icloud.com>
2024-01-16 16:36:11 +0300

    Add getCustomerById method

--- account/src/main/java/com/folksdev/account/service/CustomerService.java
+++ account/src/main/java/com/folksdev/account/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.folksdev.account.service;
 
+import com.folksdev.account.dto.CustomerDto;
+import com.folksdev.account.dto.CustomerDtoConverter;
 import com.folksdev.account.exception.CustomerNotFoundException;
 import com.folksdev.account.model.Customer;
 import com.folksdev.account.repository.CustomerRepository;
@@ -8,13 +10,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final CustomerDtoConverter customerDtoConverter;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter customerDtoConverter) {
         this.customerRepository = customerRepository;
+        this.customerDtoConverter = customerDtoConverter;
     }
 
     protected Customer findById (String id){
         return customerRepository.findById(id)
                 .orElseThrow(()->new CustomerNotFoundException("Customer could not found by id: " + id));
+    }
+
+    public CustomerDto getCustomerById(String customerId) {
+        return customerDtoConverter.convertToCustomerDto(findById(customerId));
     }
 }
