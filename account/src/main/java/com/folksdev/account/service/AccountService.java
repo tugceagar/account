7427fa0257933485095f41adf05d209bf7af91fb
tugceagar <tugceagar@icloud.com>
2024-01-16 13:30:52 +0300

    Update AccountService

--- account/src/main/java/com/folksdev/account/service/AccountService.java
+++ account/src/main/java/com/folksdev/account/service/AccountService.java
@@ -37,7 +37,8 @@ public class AccountService {
                 LocalDateTime.now());
 
         if (createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) {
-            Transaction transaction = transactionService.initiateMoney(account, createAccountRequest.getInitialCredit());
+          //  Transaction transaction = transactionService.initiateMoney(account, createAccountRequest.getInitialCredit());
+            Transaction transaction = new Transaction(createAccountRequest.getInitialCredit(),account);
             account.getTransaction().add(transaction);
         }
 
