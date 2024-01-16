9426596df501ceb18a58724a828b55b51e134670
tugceagar <tugceagar@icloud.com>
2024-01-16 16:04:09 +0300

    Remove TransactionService, triggering necessary adjustments in AccountService

--- account/src/main/java/com/folksdev/account/service/AccountService.java
+++ account/src/main/java/com/folksdev/account/service/AccountService.java
@@ -16,16 +16,13 @@ import java.time.LocalDateTime;
 public class AccountService {
     private final AccountRepository accountRepository;
     private final CustomerService customerService;
-    private final TransactionService transactionService;
     private final AccountDtoConverter converter;
 
     public AccountService(AccountRepository accountRepository,
                           CustomerService customerService,
-                          TransactionService transactionService,
                           AccountDtoConverter converter) {
         this.accountRepository = accountRepository;
         this.customerService = customerService;
-        this.transactionService = transactionService;
         this.converter = converter;
     }
 
@@ -37,8 +34,7 @@ public class AccountService {
                 LocalDateTime.now());
 
         if (createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) {
-          //  Transaction transaction = transactionService.initiateMoney(account, createAccountRequest.getInitialCredit());
-            Transaction transaction = new Transaction(createAccountRequest.getInitialCredit(),account);
+            Transaction transaction = new Transaction(createAccountRequest.getInitialCredit(), account);
             account.getTransaction().add(transaction);
         }
 
