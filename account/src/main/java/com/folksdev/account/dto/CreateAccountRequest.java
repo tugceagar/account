0e6ab8a3574c30226620cc2959ac28f6e6e30e8e
tugceagar <tugceagar@icloud.com>
2024-01-18 15:59:30 +0300

    Added validations in CreateAccountRequestDto

--- account/src/main/java/com/folksdev/account/dto/CreateAccountRequest.java
+++ account/src/main/java/com/folksdev/account/dto/CreateAccountRequest.java
@@ -1,5 +1,8 @@
 package com.folksdev.account.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +13,9 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateAccountRequest {
+    @NotBlank(message = "Customer id cannot be empty")
     private String customerId;
+    @Min(value = 0, message = "Credit cannot be negative")
     private BigDecimal initialCredit;
 
 
