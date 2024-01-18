80874f9a419399db68c82597a894df4116a886a6
tugceagar <tugceagar@icloud.com>
2024-01-18 16:00:44 +0300

    Added validation annotation

--- account/src/main/java/com/folksdev/account/controller/AccountController.java
+++ account/src/main/java/com/folksdev/account/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.folksdev.account.controller;
 import com.folksdev.account.dto.AccountDto;
 import com.folksdev.account.dto.CreateAccountRequest;
 import com.folksdev.account.service.AccountService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,7 @@ public class AccountController {
     }
 
     @PostMapping
-    public ResponseEntity<AccountDto> createAccount(@RequestBody CreateAccountRequest request){
+    public ResponseEntity<AccountDto> createAccount(@Valid @RequestBody CreateAccountRequest request){
         return ResponseEntity.ok(accountService.createAccount(request));
     }
 }
